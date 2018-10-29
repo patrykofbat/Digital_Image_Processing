364b0a18adb80ca3b678fe2a5ccbca44a6477f1e
Patryk <patrykpapior1997@gmail.com>
2018-10-29 14:20:18 +0100

    lab 3 homework done

--- lab3/binary_img.py
+++ lab3/binary_img.py
@@ -8,7 +8,9 @@ import matplotlib.gridspec as gridspec
 matplotlib.use('agg')
 
 
+
 img = cv2.imread("pointbased/100zloty.jpg", 0)
+#img = cv2.imread("pointbased/lena.bmp", 0)
 
 x, y = img.shape[:2]
 
@@ -55,6 +57,23 @@ for i in range(8):
         plt.title(str(i+1) + 'st')
 
 plt.show()
+tmp_img = cv2.add(array_of_binary[6], array_of_binary[7])
+tmp_img_2 = cv2.add(array_of_binary[4], array_of_binary[5])
+newImage = cv2.add(tmp_img, tmp_img_2)
+newImage_2 = cv2.add(array_of_binary[3], newImage)
+newImage_3 = cv2.add(array_of_binary[2], newImage_2)
+newImage_4 = cv2.add(array_of_binary[1], newImage_3)
+newImage_5 = cv2.add(array_of_binary[0], newImage_4)
 
-# cv2.imshow('Original_grey', img)
-# cv2.waitKey(0)
+
+cv2.imshow('reconstruction 6 7', tmp_img)
+cv2.imshow('reconstruction_grey 4 5', tmp_img_2)
+cv2.imshow('reconstruction_grey (4 5) (6 7)', newImage)
+cv2.imshow('reconstruction_grey (4 5) (6 7) 3', newImage_2)
+cv2.imshow('reconstruction_grey (4 5) (6 7) 3 2', newImage_3)
+cv2.imshow('reconstruction_grey (4 5) (6 7) 3 2 1', newImage_4)
+cv2.imshow('reconstruction_grey (4 5) (6 7) 3 2 1 0', newImage_5)
+
+
+
+cv2.waitKey(0)

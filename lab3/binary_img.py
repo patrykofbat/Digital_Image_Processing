a86b2d1d1e54db010dd1e927e28158fc41397a9b
Patryk <patrykpapior1997@gmail.com>
2018-10-29 13:34:32 +0100

    first part of homework lab3 done

--- lab3/binary_img.py
+++ lab3/binary_img.py
@@ -1,26 +1,60 @@
 import numpy as np
 import math
 import cv2
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+matplotlib.use('agg')
+
 
 img = cv2.imread("pointbased/100zloty.jpg", 0)
 
-#x, y = img.shape[:2]
+x, y = img.shape[:2]
 
+array_of_binary = []
 
-#img_grey = np.zeros([x, y, 3], dtype=np.uint8)
-
-
-
-# for i in range(0, x):
-#     for j in range(0, y):
-#         mean = np.mean(img[i, j])
-#         img_grey[i, j] = np.full((1, 3), mean)
+for i in range(8):
+    array_of_binary.append(np.zeros([x, y], dtype=np.uint8))
 
 
 
+for i in range(0, x):
+    for j in range(0, y):
+        bits = img[i, j]
+        counter = 128
+        sum = 0
+        for k in range(8):
+            result = bits & counter
+            if result == counter:
+                sum += result
+            array_of_binary[k][i, j] = result
+            counter = counter >> 1
+
+
+gs = gridspec.GridSpec(3, 4)
+plt.figure(1)
 
 
 
+plt.subplot(gs[0, :])
+plt.imshow(img, cmap='gray')
+plt.axis('off')
+plt.title('Original')
 
-cv2.imshow('Original_grey', img)
-cv2.waitKey(0)
+for i in range(8):
+    if i < 4:
+        plt.subplot(gs[1, i])
+        plt.imshow(array_of_binary[i], cmap='gray')
+        plt.axis('off')
+        plt.title(str(i+1) + 'st')
+    else:
+        plt.subplot(gs[2, (i - 4)])
+        plt.imshow(array_of_binary[i], cmap='gray')
+        plt.axis('off')
+        plt.title(str(i+1) + 'st')
+
+plt.show()
+
+# cv2.imshow('Original_grey', img)
+# cv2.waitKey(0)
